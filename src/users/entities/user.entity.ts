daa9a41fc145299f171ee67ccf018efb58d5c7bc
leonardo <dleonardoavella@gmail.com>
2024-09-13 20:39:56 -0500

    feat: first commit

---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,5 @@
+export class User {
+  name: string;
+  level: string;
+  initials: string;
+}
---
+++ src/users/users.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
+
+@Injectable()
+export class UsersService {
+  create(createUserDto: CreateUserDto) {
+    return 'This action adds a new user';
+  }
+
+  findAll(): User[] {
+    return [
+      {
+        name: 'Carlos Vargas',
+        level: 'Admin',
+        initials: 'CV'
+      },
+      {
+        name: 'Jorge Diaz',
+        level: 'Admin',
+        initials: 'JDM'
+      },
+    ];
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} user`;
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    return `This action updates a #${id} user`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} user`;
+  }
+}
